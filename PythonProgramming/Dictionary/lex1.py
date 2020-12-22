2bbd2881453192ca18e4df50fce8f5cc9e0ee16d
Ghazi Khan <46259869+Ghazi-Khan@users.noreply.github.com>
2020-12-22 10:20:52 +0530

    Create lex1.py

---
+++ PythonProgramming/Dictionary/lex1.py
@@ -0,0 +1,23 @@
+#lex_auth_012693774187716608134
+
+'''
+Author  : Ghazi Khan
+problem : https://lex.infosysapps.com/en/viewer/hands-on/lex_auth_012693774187716608134?collectionId=lex_auth_0125409616243425281061&collectionType=Course
+'''
+def translate(bilingual_dict,english_words_list):
+    #Write your logic here
+    swedish_words_list = []
+    for item in english_words_list:
+        # TODO: write code...
+        swedish_words_list.append(bilingual_dict[item])
+        
+    return swedish_words_list
+
+
+bilingual_dict= {"merry":"god", "christmas":"jul", "and":"och", "happy":"gott", "new":"nytt", "year":"ar"}
+english_words_list=["merry","christmas"]
+print("The bilingual dict is:",bilingual_dict)
+print("The english words are:",english_words_list)
+
+swedish_words_list=translate(bilingual_dict, english_words_list)
+print("The equivalent swedish words are:",swedish_words_list)
